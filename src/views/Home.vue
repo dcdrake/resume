655b925404f8aeabc340e869d9d67fb443a2e05e
Dylan Drake <dylan.c.drake@gmail.com>
2021-02-28 18:02:49 -0500

    border around image, and skill columns

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,34 @@
 <template>
   <b-container fluid>
-    <b-row class="justify-content-center">
-      <img
-        alt="Dylan picture"
-        style="height: 200px; width: 275px; border-radius: 10%"
-        src="../assets/dylan_shuttle_photo.jpg"
-      />
-    </b-row>
-    <b-row class="justify-content-center pb-10">
-      <h1>Dylan Drake</h1>
-    </b-row>
-    <b-row class="justify-content-center second-container">
-      <p class="lead">
-        I write code, and I'm motivated by making people feel smart, and look
-        competent using the software I develop.
-      </p>
+    <b-row class="page-header justify-content-center">
+      <b-col class="col-lg-9">
+        <b-row>
+          <b-col class="text-left col-lg-4">
+            <img
+              alt="Dylan picture"
+              class="profile-picture"
+              style="height: 200px; width: 275px; border-radius: 10%"
+              src="../assets/dylan_shuttle_photo.jpg"
+            />
+          </b-col>
+          <b-col class="text-left">
+            <h1>Dylan Drake</h1>
+            <hr />
+            <p><b-icon icon="geo-alt-fill"></b-icon> Richmond, VA</p>
+
+            <p>
+              I write code, and I'm motivated by making people feel smart, and
+              look competent using the software I develop.
+            </p>
+            <b-icon icon="envelope-fill"></b-icon>
+            <a class="mr-3" href="mailto:dylan.c.drake@gmail.com"> Email</a>
+            <b-icon icon="linkedin"></b-icon>
+            <a class="mr-3" href="https://www.linkedin.com/in/dylancdrake/">
+              LinkedIn</a
+            >
+          </b-col>
+        </b-row>
+      </b-col>
     </b-row>
     <b-row class="first-container justify-content-center">
       <b-col class="col-lg-9">
@@ -48,7 +62,7 @@
           <b-col class="text-left">
             <h4>MITRE</h4>
             <p>McLean, VA</p>
-            <p>2016-Current</p>
+            <p>2016 - Current</p>
           </b-col>
           <b-col class="text-left">
             <p><strong>Senior Software Engineer</strong></p>
@@ -65,7 +79,7 @@
           <b-col class="text-left">
             <h4>Unisys Corporation</h4>
             <p>Hampton, VA - NASA Langley Research Center</p>
-            <p>2016-Current</p>
+            <p>2014 - 2016</p>
           </b-col>
           <b-col class="text-left">
             <p><strong>Software Engineer</strong></p>
@@ -80,17 +94,19 @@
         </b-row>
       </b-col>
     </b-row>
-    <!-- <b-row class="first-container">
-      <b-col>
-        <div class="section-header">Abilities</div>
-        <b-row style="padding-bottom: 60px">
-          <b-col>
-            <h4>Tools</h4>
-            <div>Atlassian</div>
-            <div>GitLab</div>
-            <div>Jupyter</div>
+    <b-row class="first-container justify-content-center">
+      <b-col class="col-lg-9">
+        <div class="section-header text-left">Abilties</div>
+        <hr />
+        <b-row class="mb-4">
+          <b-col class="text-left">
+            <h4>Web</h4>
+            <div>VueJS</div>
+            <div>AngularJS</div>
+            <div>NodeJS</div>
+            <div>Flask</div>
           </b-col>
-          <b-col>
+          <b-col class="text-left">
             <h4>Languages</h4>
             <div>C++</div>
             <div>Python</div>
@@ -98,40 +114,33 @@
             <div>Java</div>
             <div>Kotlin</div>
           </b-col>
-          <b-col>
-            <h4>Web</h4>
-            <div>VueJS</div>
-            <div>AngularJS</div>
-            <div>NodeJS</div>
-            <div>Flask</div>
-          </b-col>
         </b-row>
-        <b-row>
-          <b-col>
+        <b-row class="mb-4">
+          <b-col class="text-left">
             <h4>Revision Control</h4>
             <div>git</div>
             <div>svn</div>
           </b-col>
-          <b-col>
-            <h4>Containerization</h4>
-            <div>Docker</div>
-          </b-col>
-          <b-col>
+          <b-col class="text-left">
             <h4>Databases</h4>
             <div>MongoDB</div>
             <div>MySQL</div>
           </b-col>
         </b-row>
+        <b-row class="mb-4">
+          <b-col class="text-left">
+            <h4>Tools</h4>
+            <div>Atlassian</div>
+            <div>GitLab</div>
+            <div>Jupyter</div>
+          </b-col>
+          <b-col class="text-left">
+            <h4>Containerization</h4>
+            <div>Docker</div>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
-    <b-row class="justify-content-center second-container">
-      <b-col>
-        <div class="section-header">Contact</div>
-        <div style="font-weight: bold; font-size: 1.5rem">
-          dylan.c.drake at gmail
-        </div>
-      </b-col>
-    </b-row> -->
   </b-container>
 </template>
 
@@ -155,6 +164,15 @@ hr {
   border-bottom: none;
   width: 30%;
   margin: 5px auto 20px 0;
+}
+
+.profile-picture {
+  border: 7px solid #f4f1de;
+}
+
+.page-header {
+  position: relative;
+  padding: 25px 25px;
 }
 
 .section-header {
